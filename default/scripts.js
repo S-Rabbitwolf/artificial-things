90b291505a29fcf6a7bddf00d6656e3e02bcb4b6
Tor de Vries <tor.devries@wsu.edu>
2026-03-23 19:31:20 +0000

    fixed misnamed function

--- default/scripts.js
+++ default/scripts.js
@@ -3,12 +3,12 @@
 Default JavaScript 
 
 Instead of document.addEventListener() with DOMContentLoaded, put your initialization code inside the "initalize()" function.
-Make sure its starting line is "export function initialize() {".
+Make sure its starting line is "export function initializePage() {" and leave in the first console log message.
 
 */
 
 // start your JS here
-export function initialize() {
+export function initializePage() {
 
     // output message to console
     console.log("Default style loaded");
