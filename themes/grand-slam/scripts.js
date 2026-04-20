5e87c2509fbf173067b879ac560b26fef7f32ecb
KippieTheBartender <146780809+KippieTheBartender@users.noreply.github.com>
2026-04-20 00:06:30 +0000

    Changed fonts and layouts. Using a 5x3 grid for layout control.

--- themes/grand-slam/scripts.js
+++ themes/grand-slam/scripts.js
@@ -10,9 +10,17 @@ Your first statement should be to call confirmTheme() with the name of your them
 */
 
 // start your JS here
-export function initializePage() {
+export function initializePage() { 
 
     // call main function that outputs message to console and update page title
     confirmTheme("Grand Slam");
 
+    let newArticle = document.createElement("article");
+    document.body.append(newArticle);
+
+    let header = document.querySelector("header");
+    let main = document.querySelector("main");
+    let footer = document.querySelector("footer");
+
+    newArticle.append(header, main, footer);
 }
